7f17a50ed208f52a43477accb234a91a73f80af7
diegonguyen86 <146827007+diegonguyen86@users.noreply.github.com>
2026-05-22 12:31:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss(),
-    // --- 🛡️ LỆNH TỰ HỦY PWA TẬN GỐC ---
-    VitePWA({
-      selfDestroying: true, // Khi điện thoại nhận được bản build này, nó sẽ tự xóa Service Worker
-    })
+    tailwindcss()
   ],
   base: "/", 
   server: {
